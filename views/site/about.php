d2bf615139b0cc0fb0c3764ed9d6a4593d30d7ee
denishom12 <36245920+denishom12@users.noreply.github.com>
2020-04-06 12:08:05 +0300

    Создали модель для колонки из фильмов bbbb

---
+++ views/site/about.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1>About company</h1>
+
+    <p>
+        This is about company
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
